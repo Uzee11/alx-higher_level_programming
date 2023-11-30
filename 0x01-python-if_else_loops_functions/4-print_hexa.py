7d8ec18415b635550f434ffbb75985e74c894e84
Uzee11 <uzeeokoye@yahoo.com>
2023-11-30 04:46:00 -0800

    pythonproject

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
-
 #!/usr/bin/python3
 
 for i in range(99):
-    print("{:d} {}".format(i, hex(i)))i
+    # Using format() to print in decimal and hexadecimal
+    print("{} = {}".format(i, hex(i)))
