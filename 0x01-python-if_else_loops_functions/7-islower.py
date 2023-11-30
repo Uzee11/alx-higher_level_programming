7aaf78a26471a303599f62e8f1d5328c2c932d07
Uzee11 <uzeeokoye@yahoo.com>
2023-11-30 05:50:52 -0800

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def islower(c):
     # Check if the input is a single character and if it is lowercase
-    if len(c) == 1 and 'a' <= c <= 'z':
+    if isinstance(c, str) and len(c) == 1 and 'a' <= c <= 'z':
         return f"{c} => lower"
     else:
-        return f"{c} => not lower"
+        return "Invalid input"
 
 # Example usage:
 character = 'a'
