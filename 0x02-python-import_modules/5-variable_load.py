3a8865e5baf84d1c5b3cdd821df8c79b163ca4a4
Uzee11 <uzeeokoye@yahoo.com>
2023-12-01 04:52:00 -0800

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
-import importlib
-
-def print_variable_a():
-    variable_module = importlib.import_module("variable_load_5")
-    print(variable_module.a)
+# 5-variable_load.py
 
 if __name__ == "__main__":
-    print_variable_a()
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
