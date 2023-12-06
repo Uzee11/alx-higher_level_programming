4b162ee97a4fd52a26eb93cd096ad0e7bbcdd9de
Uzee11 <uzeeokoye@yahoo.com>
2023-12-06 12:05:11 -0800

    more data

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
+# 0-square_matrix_simple.py
+
 
 def square_matrix_simple(matrix=[]):
-    # Create a new matrix with the same size as the input matrix
-    result_matrix = [[x**2 for x in row] for row in matrix]
-    return result_matrix
-
-# Example usage:
-input_matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-output_matrix = square_matrix_simple(input_matrix)
-print(output_matrix)
+    """Compute the square value of all integers of a matrix."""
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
