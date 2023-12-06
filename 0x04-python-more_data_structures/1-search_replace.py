ef47af3cf49740c20ef85d4e667b4e8d99ea6eb0
Uzee11 <uzeeokoye@yahoo.com>
2023-12-06 12:22:49 -0800

    0x04-python-data

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
+# 1-search_replace.py
+
 
 def search_replace(my_list, search, replace):
-    # Create a new list with the replaced elements
-    new_list = [replace if element == search else element for element in my_list]
-    return new_list
-
-# Example usage:
-original_list = [1, 2, 3, 2, 4, 2, 5]
-element_to_replace = 2
-replacement_element = 99
-
-new_list = search_replace(original_list, element_to_replace, replacement_element)
-print(new_list)
+    """Replace all occurrences of an element by another in a new list."""
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
