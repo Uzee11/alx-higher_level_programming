b4c1ba97abe5a98e75460d40a0bee1fb5065c2b4
Uzee11 <uzeeokoye@yahoo.com>
2023-12-18 21:08:35 -0800

    safeprint

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+def safe_print_division(a, b):
+    """Returns the division of a by b."""
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
