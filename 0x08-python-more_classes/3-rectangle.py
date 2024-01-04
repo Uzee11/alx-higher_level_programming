b8e1e2bdc45b33a2a4adc6a77678e81759e17c92
Uzee11 <uzeeokoye@yahoo.com>
2024-01-04 02:12:25 -0800

    3rectangle

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """Class that defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialization method with optional width and height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter method for width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Getter method for height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Public method to calculate and return the rectangle area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Public method to calculate and return the rectangle perimeter."""
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """String representation of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        
+        rectangle_str = ""
+        for _ in range(self.__height):
+            rectangle_str += '#' * self.__width + '\n'
+        return rectangle_str.rstrip()
+
+    def __repr__(self):
+        """Representation of the rectangle."""
+        return f"Rectangle({self.__width}, {self.__height})"
